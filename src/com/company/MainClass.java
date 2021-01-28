91a187784400b6a746a2aca7ba70e7112249f15a
burju <kanliska@gmail.com>
2021-01-28 14:34:10 -0600

    a new solar system created on main and specs added

--- src/com/company/MainClass.java
+++ src/com/company/MainClass.java
@@ -4,6 +4,9 @@ public class MainClass {
     public static void main(String[] args) {
 
         SolarSystem mySolarSystem= new SolarSystem();
-        
+        mySolarSystem.moons=5;
+        mySolarSystem.planets=27;
+        mySolarSystem.sun=1;
+
     }
 }
